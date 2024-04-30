aedb92e850718405f29d2d7fe1981e1e71116d98
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-04-30 15:45:42 -0600

    source.sh

--- source.sh
+++ source.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Create a new Google Cloud project
-gcloud projects create lualmedele-source --name="Lualmedele-Source"
+gcloud projects create finn-partners-source --name="Finn-Partners-Source"
 
 # Set the current Google Cloud project
-gcloud config set project lualmedele-source
+gcloud config set project finn-partners-source
 
 # Create a new service account
-gcloud iam service-accounts create Lualmedele-Source --project=lualmedele-source
+gcloud iam service-accounts create Finn-Partners-Source --project=finn-partners-source
 
 # Add IAM policy binding to the project
-gcloud projects add-iam-policy-binding lualmedele-source --member="serviceAccount:Lualmedele-Source@lualmedele-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding finn-partners-source --member="serviceAccount:Finn-Partners-Source@finn-partners-source.iam.gserviceaccount.com" --role="roles/editor"
 
 # Get the unique ID of the service account
-gcloud iam service-accounts describe Lualmedele-Source@lualmedele-source.iam.gserviceaccount.com --project=lualmedele-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Finn-Partners-Source@finn-partners-source.iam.gserviceaccount.com --project=finn-partners-source --format="value(uniqueId)"
 
 # Create a service account key and save it to a JSON file
-gcloud iam service-accounts keys create Lualmedele-Source.json --iam-account=Lualmedele-Source@lualmedele-source.iam.gserviceaccount.com --project=lualmedele-source
+gcloud iam service-accounts keys create Finn-Partners-Source.json --iam-account=Finn-Partners-Source@finn-partners-source.iam.gserviceaccount.com --project=finn-partners-source
 
 # Enable necessary Google services
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=lualmedele-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=finn-partners-source
 
 # Download the service account key JSON file
-cloudshell download Lualmedele-Source.json
+cloudshell download Finn-Partners-Source.json
