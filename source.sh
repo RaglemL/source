33b87464b40e596e6cf43c6ef9f317a16fbcfd7e
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-05-07 11:48:41 -0600

    source.sh

--- source.sh
+++ source.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Create a new Google Cloud project
-gcloud projects create avenue_z-source --name="Avenue_Z-Source"
+gcloud projects create avenue-z-source --name="Avenue-Z-Source"
 
 # Set the current Google Cloud project
-gcloud config set project avenue_z-source
+gcloud config set project avenue-z-source
 
 # Create a new service account
-gcloud iam service-accounts create Avenue_Z-Source --project=avenue_z-source
+gcloud iam service-accounts create Avenue-Z-Source --project=avenue-z-source
 
 # Add IAM policy binding to the project
-gcloud projects add-iam-policy-binding avenue_z-source --member="serviceAccount:Avenue_Z-Source@avenue_z-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding avenue-z-source --member="serviceAccount:Avenue-Z-Source@avenue-z-source.iam.gserviceaccount.com" --role="roles/editor"
 
 # Get the unique ID of the service account
-gcloud iam service-accounts describe Avenue_Z-Source@avenue_z-source.iam.gserviceaccount.com --project=avenue_z-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Avenue-Z-Source@avenue-z-source.iam.gserviceaccount.com --project=avenue-z-source --format="value(uniqueId)"
 
 # Create a service account key and save it to a JSON file
-gcloud iam service-accounts keys create Avenue_Z-Source.json --iam-account=Avenue_Z-Source@avenue_z-source.iam.gserviceaccount.com --project=avenue_z-source
+gcloud iam service-accounts keys create Avenue-Z-Source.json --iam-account=Avenue-Z-Source@avenue-z-source.iam.gserviceaccount.com --project=avenue-z-source
 
 # Enable necessary Google services
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=avenue_z-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=avenue-z-source
 
 # Download the service account key JSON file
-cloudshell download Avenue_Z-Source.json
+cloudshell download Avenue-Z-Source.json
