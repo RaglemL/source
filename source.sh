2c3d7fa2bf0037264350ae7caab280f5e8f30325
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-05-24 09:57:46 -0600

    source.sh

--- source.sh
+++ source.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Create a new Google Cloud project
-gcloud projects create mysalesdeskco-source --name="Mysalesdeskco-Source"
+gcloud projects create rollingsuds-source --name="Rollingsuds-Source"
 
 # Set the current Google Cloud project
-gcloud config set project mysalesdeskco-source
+gcloud config set project rollingsuds-source
 
 # Create a new service account
-gcloud iam service-accounts create Mysalesdeskco-Source --project=mysalesdeskco-source
+gcloud iam service-accounts create Rollingsuds-Source --project=rollingsuds-source
 
 # Add IAM policy binding to the project
-gcloud projects add-iam-policy-binding mysalesdeskco-source --member="serviceAccount:Mysalesdeskco-Source@mysalesdeskco-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding rollingsuds-source --member="serviceAccount:Rollingsuds-Source@rollingsuds-source.iam.gserviceaccount.com" --role="roles/editor"
 
 # Get the unique ID of the service account
-gcloud iam service-accounts describe Mysalesdeskco-Source@mysalesdeskco-source.iam.gserviceaccount.com --project=mysalesdeskco-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Rollingsuds-Source@rollingsuds-source.iam.gserviceaccount.com --project=rollingsuds-source --format="value(uniqueId)"
 
 # Create a service account key and save it to a JSON file
-gcloud iam service-accounts keys create Mysalesdeskco-Source.json --iam-account=Mysalesdeskco-Source@mysalesdeskco-source.iam.gserviceaccount.com --project=mysalesdeskco-source
+gcloud iam service-accounts keys create Rollingsuds-Source.json --iam-account=Rollingsuds-Source@rollingsuds-source.iam.gserviceaccount.com --project=rollingsuds-source
 
 # Enable necessary Google services
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=mysalesdeskco-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=rollingsuds-source
 
 # Download the service account key JSON file
-cloudshell download Mysalesdeskco-Source.json
+cloudshell download Rollingsuds-Source.json
