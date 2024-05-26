32780d675eb1505824d14a27fa548cefb67ed911
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-05-26 02:13:43 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Create a new Google Cloud project
-gcloud projects create alavergaperro-source --name="Alavergaperro-Source"
+gcloud projects create chupaladoggo-source --name="Chupaladoggo-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
-gcloud config set project alavergaperro-source
+gcloud config set project chupaladoggo-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
@@ -18,27 +18,27 @@ gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKe
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a new service account
-gcloud iam service-accounts create Alavergaperro-Source --project=alavergaperro-source
+gcloud iam service-accounts create Chupaladoggo-Source --project=chupaladoggo-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
-gcloud projects add-iam-policy-binding alavergaperro-source --member="serviceAccount:Alavergaperro-Source@alavergaperro-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding chupaladoggo-source --member="serviceAccount:Chupaladoggo-Source@chupaladoggo-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
-gcloud iam service-accounts describe Alavergaperro-Source@alavergaperro-source.iam.gserviceaccount.com --project=alavergaperro-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Chupaladoggo-Source@chupaladoggo-source.iam.gserviceaccount.com --project=chupaladoggo-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
-gcloud iam service-accounts keys create Alavergaperro-Source.json --iam-account=Alavergaperro-Source@alavergaperro-source.iam.gserviceaccount.com --project=alavergaperro-source
+gcloud iam service-accounts keys create Chupaladoggo-Source.json --iam-account=Chupaladoggo-Source@chupaladoggo-source.iam.gserviceaccount.com --project=chupaladoggo-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=alavergaperro-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com --project=chupaladoggo-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
-cloudshell download Alavergaperro-Source.json
+cloudshell download Chupaladoggo-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
