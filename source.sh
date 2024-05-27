d63746c2ab24fed79b029e87622dd48ee7e20a60
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-05-27 11:18:43 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,12 +2,12 @@
 
 # Create a new Google Cloud project
 echo "Creating GCP Project..."
-gcloud projects create rollingsudspittsburgh-source --name="Rollingsudspittsburgh-Source"
+gcloud projects create rollingsudsbaltimore-source --name="Rollingsudsbaltimore-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "Setting GCP Project..."
-gcloud config set project rollingsudspittsburgh-source
+gcloud config set project rollingsudsbaltimore-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
@@ -24,32 +24,32 @@ sleep 10 # Adding a delay of 5 seconds
 
 # Create a new service account
 echo "Creating Service Account..."
-gcloud iam service-accounts create Rollingsudspittsburgh-Source --project=rollingsudspittsburgh-source
+gcloud iam service-accounts create Rollingsudsbaltimore-Source --project=rollingsudsbaltimore-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "Adding Policies..."
-gcloud projects add-iam-policy-binding rollingsudspittsburgh-source --member="serviceAccount:Rollingsudspittsburgh-Source@rollingsudspittsburgh-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding rollingsudsbaltimore-source --member="serviceAccount:Rollingsudsbaltimore-Source@rollingsudsbaltimore-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "Obtaining Unique ID..."
-gcloud iam service-accounts describe Rollingsudspittsburgh-Source@rollingsudspittsburgh-source.iam.gserviceaccount.com --project=rollingsudspittsburgh-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Rollingsudsbaltimore-Source@rollingsudsbaltimore-source.iam.gserviceaccount.com --project=rollingsudsbaltimore-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "Creating JSON Key..."
-gcloud iam service-accounts keys create Rollingsudspittsburgh-Source.json --iam-account=Rollingsudspittsburgh-Source@rollingsudspittsburgh-source.iam.gserviceaccount.com --project=rollingsudspittsburgh-source
+gcloud iam service-accounts keys create Rollingsudsbaltimore-Source.json --iam-account=Rollingsudsbaltimore-Source@rollingsudsbaltimore-source.iam.gserviceaccount.com --project=rollingsudsbaltimore-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "Enabling APIs..."
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=rollingsudspittsburgh-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=rollingsudsbaltimore-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "Downloading JSON Key..."
-cloudshell download Rollingsudspittsburgh-Source.json
+cloudshell download Rollingsudsbaltimore-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
