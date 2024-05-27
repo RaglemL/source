8f1c62bd5dbd291d76d06528b6b65df5f9623432
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-05-26 21:04:43 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,19 +2,19 @@
 
 # Create a new Google Cloud project
 echo "Creating GCP Project..."
-gcloud projects create rollingsudsco-source --name="Rollingsudsco-Source"
+gcloud projects create rollingsudspw-source --name="Rollingsudspw-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "Setting GCP Project..."
-gcloud config set project rollingsudsco-source
+gcloud config set project rollingsudspw-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "Assigning Roles..."
-gcloud organizations add-iam-policy-binding 933898662270 --member="user:cloudasta@rollingsudsco.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 933898662270 --member="user:cloudasta@rollingsudspw.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 933898662270 --member="user:cloudasta@rollingsudsco.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 933898662270 --member="user:cloudasta@rollingsudspw.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
@@ -24,32 +24,32 @@ sleep 5 # Adding a delay of 5 seconds
 
 # Create a new service account
 echo "Creating Service Account..."
-gcloud iam service-accounts create Rollingsudsco-Source --project=rollingsudsco-source
+gcloud iam service-accounts create Rollingsudspw-Source --project=rollingsudspw-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "Adding Policies..."
-gcloud projects add-iam-policy-binding rollingsudsco-source --member="serviceAccount:Rollingsudsco-Source@rollingsudsco-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding rollingsudspw-source --member="serviceAccount:Rollingsudspw-Source@rollingsudspw-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "Obtaining Unique ID..."
-gcloud iam service-accounts describe Rollingsudsco-Source@rollingsudsco-source.iam.gserviceaccount.com --project=rollingsudsco-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Rollingsudspw-Source@rollingsudspw-source.iam.gserviceaccount.com --project=rollingsudspw-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "Creating JSON Key..."
-gcloud iam service-accounts keys create Rollingsudsco-Source.json --iam-account=Rollingsudsco-Source@rollingsudsco-source.iam.gserviceaccount.com --project=rollingsudsco-source
+gcloud iam service-accounts keys create Rollingsudspw-Source.json --iam-account=Rollingsudspw-Source@rollingsudspw-source.iam.gserviceaccount.com --project=rollingsudspw-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "Enabling APIs..."
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=rollingsudsco-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=rollingsudspw-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "Downloading JSON Key..."
-cloudshell download Rollingsudsco-Source.json
+cloudshell download Rollingsudspw-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
