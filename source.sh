6f08e58cb73973b9028190fda2172f144080ff9b
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-03 16:31:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -12,9 +12,9 @@ sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 322861720586 --member="user:cloudasta@rollingsudpowerwash.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 322861720586 --member="user:cloudasta@archive.rollingsudpowerwash.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 322861720586 --member="user:cloudasta@rollingsudpowerwash.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 322861720586 --member="user:cloudasta@archive.rollingsudpowerwash.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
