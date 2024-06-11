88ee7f99986d73aa5b986c64838ddd766805a2b7
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-11 10:01:45 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create anago-mailnet-source --name="Anago-Mailnet-Source"
+gcloud projects create go-anago-source --name="Go-Anago-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project anago-mailnet-source
+gcloud config set project go-anago-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 1026486340178 --member="user:cloudasta@archive.anago-mail.net" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 803112670531 --member="user:cloudasta@archive.go-anago.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 1026486340178 --member="user:cloudasta@archive.anago-mail.net" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 803112670531 --member="user:cloudasta@archive.go-anago.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=1026486340178
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=803112670531
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Anago-Mailnet-Source --project=anago-mailnet-source
+gcloud iam service-accounts create Go-Anago-Source --project=go-anago-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding anago-mailnet-source --member="serviceAccount:Anago-Mailnet-Source@anago-mailnet-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding go-anago-source --member="serviceAccount:Go-Anago-Source@go-anago-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Anago-Mailnet-Source@anago-mailnet-source.iam.gserviceaccount.com --project=anago-mailnet-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Go-Anago-Source@go-anago-source.iam.gserviceaccount.com --project=go-anago-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Anago-Mailnet-Source.json --iam-account=Anago-Mailnet-Source@anago-mailnet-source.iam.gserviceaccount.com --project=anago-mailnet-source
+gcloud iam service-accounts keys create Go-Anago-Source.json --iam-account=Go-Anago-Source@go-anago-source.iam.gserviceaccount.com --project=go-anago-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=anago-mailnet-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=go-anago-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Anago-Mailnet-Source.json
+cloudshell download Go-Anago-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=1026486340178
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=803112670531
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
