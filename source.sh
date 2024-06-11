0451d984b7d2374829501147cf14b6ded548b896
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-10 20:28:45 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,19 +2,19 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create rollingsudstulsa-source --name="Rollingsudstulsa-Source"
+gcloud projects create droneracingleague-source --name="Droneracingleague-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project rollingsudstulsa-source
+gcloud config set project droneracingleague-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 704053694919 --member="user:cloudasta@archive.rollingsudstulsa.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 704053694919 --member="user:cloudasta@droneracingleague.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 704053694919 --member="user:cloudasta@archive.rollingsudstulsa.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 704053694919 --member="user:cloudasta@droneracingleague.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
@@ -24,32 +24,32 @@ sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Rollingsudstulsa-Source --project=rollingsudstulsa-source
+gcloud iam service-accounts create Droneracingleague-Source --project=droneracingleague-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding rollingsudstulsa-source --member="serviceAccount:Rollingsudstulsa-Source@rollingsudstulsa-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding droneracingleague-source --member="serviceAccount:Droneracingleague-Source@droneracingleague-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Rollingsudstulsa-Source@rollingsudstulsa-source.iam.gserviceaccount.com --project=rollingsudstulsa-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Droneracingleague-Source@droneracingleague-source.iam.gserviceaccount.com --project=droneracingleague-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Rollingsudstulsa-Source.json --iam-account=Rollingsudstulsa-Source@rollingsudstulsa-source.iam.gserviceaccount.com --project=rollingsudstulsa-source
+gcloud iam service-accounts keys create Droneracingleague-Source.json --iam-account=Droneracingleague-Source@droneracingleague-source.iam.gserviceaccount.com --project=droneracingleague-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=rollingsudstulsa-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=droneracingleague-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Rollingsudstulsa-Source.json
+cloudshell download Droneracingleague-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
