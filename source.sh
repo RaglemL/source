2c50735a004d080884b6d15bc6cdf0cafdea3a63
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-17 10:53:45 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create landmarklandscapegroup-source --name="Landmarklandscapegroup-Source"
+gcloud projects create supplypointemail-source --name="Supplypointemail-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project landmarklandscapegroup-source
+gcloud config set project supplypointemail-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 917102726294 --member="user:cloudasta@archive.landmarklandscapegroup.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 243278845904 --member="user:cloudasta@supplypointemail.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 917102726294 --member="user:cloudasta@archive.landmarklandscapegroup.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 243278845904 --member="user:cloudasta@supplypointemail.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=917102726294
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=243278845904
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Landmarklandscapegroup-Source --project=landmarklandscapegroup-source
+gcloud iam service-accounts create Supplypointemail-Source --project=supplypointemail-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding landmarklandscapegroup-source --member="serviceAccount:Landmarklandscapegroup-Source@landmarklandscapegroup-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding supplypointemail-source --member="serviceAccount:Supplypointemail-Source@supplypointemail-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Landmarklandscapegroup-Source@landmarklandscapegroup-source.iam.gserviceaccount.com --project=landmarklandscapegroup-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Supplypointemail-Source@supplypointemail-source.iam.gserviceaccount.com --project=supplypointemail-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Landmarklandscapegroup-Source.json --iam-account=Landmarklandscapegroup-Source@landmarklandscapegroup-source.iam.gserviceaccount.com --project=landmarklandscapegroup-source
+gcloud iam service-accounts keys create Supplypointemail-Source.json --iam-account=Supplypointemail-Source@supplypointemail-source.iam.gserviceaccount.com --project=supplypointemail-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=landmarklandscapegroup-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=supplypointemail-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Landmarklandscapegroup-Source.json
+cloudshell download Supplypointemail-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=917102726294
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=243278845904
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
