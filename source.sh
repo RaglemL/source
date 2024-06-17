02d9f82cc999a649f957e54e9ca5211ac67fd361
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-17 12:57:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create freedomfms-source --name="Freedomfms-Source"
+gcloud projects create mightydogemail-source --name="Mightydogemail-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project freedomfms-source
+gcloud config set project mightydogemail-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 543841303790 --member="user:cloudasta@freedomfms.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 97876285718 --member="user:cloudasta@mightydogemail.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 543841303790 --member="user:cloudasta@freedomfms.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 97876285718 --member="user:cloudasta@mightydogemail.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=543841303790
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=97876285718
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Freedomfms-Source --project=freedomfms-source
+gcloud iam service-accounts create Mightydogemail-Source --project=mightydogemail-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding freedomfms-source --member="serviceAccount:Freedomfms-Source@freedomfms-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding mightydogemail-source --member="serviceAccount:Mightydogemail-Source@mightydogemail-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Freedomfms-Source@freedomfms-source.iam.gserviceaccount.com --project=freedomfms-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Mightydogemail-Source@mightydogemail-source.iam.gserviceaccount.com --project=mightydogemail-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Freedomfms-Source.json --iam-account=Freedomfms-Source@freedomfms-source.iam.gserviceaccount.com --project=freedomfms-source
+gcloud iam service-accounts keys create Mightydogemail-Source.json --iam-account=Mightydogemail-Source@mightydogemail-source.iam.gserviceaccount.com --project=mightydogemail-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=freedomfms-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=mightydogemail-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Freedomfms-Source.json
+cloudshell download Mightydogemail-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=543841303790
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=97876285718
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
