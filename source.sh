58330e5e936cb225fe5ce9a51ec9b03584786cf5
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-20 10:07:45 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create goalpha6-source --name="Goalpha6-Source"
+gcloud projects create gslmgmt-source --name="Gslmgmt-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project goalpha6-source
+gcloud config set project gslmgmt-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 576687528287 --member="user:cloudasta@archive.goalpha6.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 768347713444 --member="user:cloudasta@archive.gslmgmt.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 576687528287 --member="user:cloudasta@archive.goalpha6.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 768347713444 --member="user:cloudasta@archive.gslmgmt.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=576687528287
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=768347713444
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Goalpha6-Source --project=goalpha6-source
+gcloud iam service-accounts create Gslmgmt-Source --project=gslmgmt-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding goalpha6-source --member="serviceAccount:Goalpha6-Source@goalpha6-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding gslmgmt-source --member="serviceAccount:Gslmgmt-Source@gslmgmt-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Goalpha6-Source@goalpha6-source.iam.gserviceaccount.com --project=goalpha6-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Gslmgmt-Source@gslmgmt-source.iam.gserviceaccount.com --project=gslmgmt-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Goalpha6-Source.json --iam-account=Goalpha6-Source@goalpha6-source.iam.gserviceaccount.com --project=goalpha6-source
+gcloud iam service-accounts keys create Gslmgmt-Source.json --iam-account=Gslmgmt-Source@gslmgmt-source.iam.gserviceaccount.com --project=gslmgmt-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=goalpha6-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=gslmgmt-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Goalpha6-Source.json
+cloudshell download Gslmgmt-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=576687528287
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=768347713444
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
