df6efb9ee2753b77df309c262b0329cc5a12ee6b
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-24 17:16:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create govensure-source --name="Govensure-Source"
+gcloud projects create bateslandscape-source --name="Bateslandscape-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project govensure-source
+gcloud config set project bateslandscape-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 590814260255 --member="user:cloudasta@govensure.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 1067978937315 --member="user:cloudasta@bateslandscape.net" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 590814260255 --member="user:cloudasta@govensure.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 1067978937315 --member="user:cloudasta@bateslandscape.net" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=590814260255
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=1067978937315
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Govensure-Source --project=govensure-source
+gcloud iam service-accounts create Bateslandscape-Source --project=bateslandscape-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding govensure-source --member="serviceAccount:Govensure-Source@govensure-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding bateslandscape-source --member="serviceAccount:Bateslandscape-Source@bateslandscape-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Govensure-Source@govensure-source.iam.gserviceaccount.com --project=govensure-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Bateslandscape-Source@bateslandscape-source.iam.gserviceaccount.com --project=bateslandscape-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Govensure-Source.json --iam-account=Govensure-Source@govensure-source.iam.gserviceaccount.com --project=govensure-source
+gcloud iam service-accounts keys create Bateslandscape-Source.json --iam-account=Bateslandscape-Source@bateslandscape-source.iam.gserviceaccount.com --project=bateslandscape-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=govensure-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=bateslandscape-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Govensure-Source.json
+cloudshell download Bateslandscape-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=590814260255
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=1067978937315
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
