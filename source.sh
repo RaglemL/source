9b05ccfb50191f324f601bc46ac88e82318f5e00
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-25 09:43:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,12 +2,12 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create groovyhuesservices-source --name="Groovyhuesservices-Source"
+gcloud projects create govensuregroup-source --name="Govensuregroup-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project groovyhuesservices-source
+gcloud config set project govensuregroup-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
@@ -24,32 +24,32 @@ sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Groovyhuesservices-Source --project=groovyhuesservices-source
+gcloud iam service-accounts create Govensuregroup-Source --project=govensuregroup-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding groovyhuesservices-source --member="serviceAccount:Groovyhuesservices-Source@groovyhuesservices-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding govensuregroup-source --member="serviceAccount:Govensuregroup-Source@govensuregroup-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Groovyhuesservices-Source@groovyhuesservices-source.iam.gserviceaccount.com --project=groovyhuesservices-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Govensuregroup-Source@govensuregroup-source.iam.gserviceaccount.com --project=govensuregroup-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Groovyhuesservices-Source.json --iam-account=Groovyhuesservices-Source@groovyhuesservices-source.iam.gserviceaccount.com --project=groovyhuesservices-source
+gcloud iam service-accounts keys create Govensuregroup-Source.json --iam-account=Govensuregroup-Source@govensuregroup-source.iam.gserviceaccount.com --project=govensuregroup-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=groovyhuesservices-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=govensuregroup-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Groovyhuesservices-Source.json
+cloudshell download Govensuregroup-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
