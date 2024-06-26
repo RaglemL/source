ad49537106c16d0f0287c413b6dc1d1cd57b142f
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-26 12:35:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create yourfrandesk-source --name="Yourfrandesk-Source"
+gcloud projects create gomaldonadolandscape-source --name="Gomaldonadolandscape-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project yourfrandesk-source
+gcloud config set project gomaldonadolandscape-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 335440807607 --member="user:cloudasta@archive.yourfrandesk.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 398590384310 --member="user:cloudasta@archive.gomaldonadolandscape.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 335440807607 --member="user:cloudasta@archive.yourfrandesk.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 398590384310 --member="user:cloudasta@archive.gomaldonadolandscape.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=335440807607
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=398590384310
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Yourfrandesk-Source --project=yourfrandesk-source
+gcloud iam service-accounts create Gomaldonadolandscape-Source --project=gomaldonadolandscape-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding yourfrandesk-source --member="serviceAccount:Yourfrandesk-Source@yourfrandesk-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding gomaldonadolandscape-source --member="serviceAccount:Gomaldonadolandscape-Source@gomaldonadolandscape-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Yourfrandesk-Source@yourfrandesk-source.iam.gserviceaccount.com --project=yourfrandesk-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Gomaldonadolandscape-Source@gomaldonadolandscape-source.iam.gserviceaccount.com --project=gomaldonadolandscape-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Yourfrandesk-Source.json --iam-account=Yourfrandesk-Source@yourfrandesk-source.iam.gserviceaccount.com --project=yourfrandesk-source
+gcloud iam service-accounts keys create Gomaldonadolandscape-Source.json --iam-account=Gomaldonadolandscape-Source@gomaldonadolandscape-source.iam.gserviceaccount.com --project=gomaldonadolandscape-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=yourfrandesk-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=gomaldonadolandscape-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Yourfrandesk-Source.json
+cloudshell download Gomaldonadolandscape-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=335440807607
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=398590384310
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
