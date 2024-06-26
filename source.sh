4cf67add4fc6e1eb476a9a37ed9b0009c00c85ef
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-26 14:41:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create gomaldonado-source --name="Gomaldonado-Source"
+gcloud projects create gosomervillecleaning-source --name="Gosomervillecleaning-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project gomaldonado-source
+gcloud config set project gosomervillecleaning-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 1064492250266 --member="user:cloudasta@archive.gomaldonado.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 365363815270 --member="user:cloudasta@archive.gosomervillecleaning.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 1064492250266 --member="user:cloudasta@archive.gomaldonado.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 365363815270 --member="user:cloudasta@archive.gosomervillecleaning.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=1064492250266
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=365363815270
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Gomaldonado-Source --project=gomaldonado-source
+gcloud iam service-accounts create Gosomervillecleaning-Source --project=gosomervillecleaning-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding gomaldonado-source --member="serviceAccount:Gomaldonado-Source@gomaldonado-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding gosomervillecleaning-source --member="serviceAccount:Gosomervillecleaning-Source@gosomervillecleaning-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Gomaldonado-Source@gomaldonado-source.iam.gserviceaccount.com --project=gomaldonado-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Gosomervillecleaning-Source@gosomervillecleaning-source.iam.gserviceaccount.com --project=gosomervillecleaning-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Gomaldonado-Source.json --iam-account=Gomaldonado-Source@gomaldonado-source.iam.gserviceaccount.com --project=gomaldonado-source
+gcloud iam service-accounts keys create Gosomervillecleaning-Source.json --iam-account=Gosomervillecleaning-Source@gosomervillecleaning-source.iam.gserviceaccount.com --project=gosomervillecleaning-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=gomaldonado-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=gosomervillecleaning-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Gomaldonado-Source.json
+cloudshell download Gosomervillecleaning-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=1064492250266
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=365363815270
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
