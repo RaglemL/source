0117a87058b41089036c34f6ab0202b61603c442
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-06-26 15:14:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create fitnessmachinetech-source --name="Fitnessmachinetech-Source"
+gcloud projects create fitmachinetechs-source --name="Fitmachinetechs-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project fitnessmachinetech-source
+gcloud config set project fitmachinetechs-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 854474156184 --member="user:cloudasta@archive.fitnessmachinetech.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 1095505130606 --member="user:cloudasta@archive.fitmachinetechs.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 854474156184 --member="user:cloudasta@archive.fitnessmachinetech.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 1095505130606 --member="user:cloudasta@archive.fitmachinetechs.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=854474156184
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=1095505130606
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Fitnessmachinetech-Source --project=fitnessmachinetech-source
+gcloud iam service-accounts create Fitmachinetechs-Source --project=fitmachinetechs-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding fitnessmachinetech-source --member="serviceAccount:Fitnessmachinetech-Source@fitnessmachinetech-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding fitmachinetechs-source --member="serviceAccount:Fitmachinetechs-Source@fitmachinetechs-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Fitnessmachinetech-Source@fitnessmachinetech-source.iam.gserviceaccount.com --project=fitnessmachinetech-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Fitmachinetechs-Source@fitmachinetechs-source.iam.gserviceaccount.com --project=fitmachinetechs-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Fitnessmachinetech-Source.json --iam-account=Fitnessmachinetech-Source@fitnessmachinetech-source.iam.gserviceaccount.com --project=fitnessmachinetech-source
+gcloud iam service-accounts keys create Fitmachinetechs-Source.json --iam-account=Fitmachinetechs-Source@fitmachinetechs-source.iam.gserviceaccount.com --project=fitmachinetechs-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=fitnessmachinetech-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=fitmachinetechs-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Fitnessmachinetech-Source.json
+cloudshell download Fitmachinetechs-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=854474156184
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=1095505130606
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
