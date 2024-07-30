8c73f43d6b2b501b3d2ce8a021f6848e931143d1
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-07-30 14:26:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,19 +2,19 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create mcas-source --name="Mcas-Source"
+gcloud projects create ecospace-source --name="Ecospace-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project mcas-source
+gcloud config set project ecospace-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 1062090734422 --member="user:cloudasta@mayfaircabinetandstone.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 1062090734422 --member="user:cloudasta@ecospace.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 1062090734422 --member="user:cloudasta@mayfaircabinetandstone.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 1062090734422 --member="user:cloudasta@ecospace.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
@@ -24,32 +24,32 @@ sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Mcas-Source --project=mcas-source
+gcloud iam service-accounts create Ecospace-Source --project=ecospace-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding mcas-source --member="serviceAccount:Mcas-Source@mcas-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding ecospace-source --member="serviceAccount:Ecospace-Source@ecospace-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Mcas-Source@mcas-source.iam.gserviceaccount.com --project=mcas-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Ecospace-Source@ecospace-source.iam.gserviceaccount.com --project=ecospace-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Mcas-Source.json --iam-account=Mcas-Source@mcas-source.iam.gserviceaccount.com --project=mcas-source
+gcloud iam service-accounts keys create Ecospace-Source.json --iam-account=Ecospace-Source@ecospace-source.iam.gserviceaccount.com --project=ecospace-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=mcas-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=ecospace-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Mcas-Source.json
+cloudshell download Ecospace-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
