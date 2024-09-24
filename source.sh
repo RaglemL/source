9639c76263e5defa71616b8f91b1b68160898a85
RaglemL <133904396+RaglemL@users.noreply.github.com>
2024-09-24 12:48:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create mindzvue-source --name="Mindzvue-Source"
+gcloud projects create landvault-source --name="Landvault-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project mindzvue-source
+gcloud config set project landvault-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 883905654251 --member="user:cloudasta@mindzvue.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 96558887780 --member="user:cloudasta@landvault.io" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 883905654251 --member="user:cloudasta@mindzvue.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 96558887780 --member="user:cloudasta@landvault.io" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=883905654251
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=96558887780
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Mindzvue-Source --project=mindzvue-source
+gcloud iam service-accounts create Landvault-Source --project=landvault-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding mindzvue-source --member="serviceAccount:Mindzvue-Source@mindzvue-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding landvault-source --member="serviceAccount:Landvault-Source@landvault-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Mindzvue-Source@mindzvue-source.iam.gserviceaccount.com --project=mindzvue-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Landvault-Source@landvault-source.iam.gserviceaccount.com --project=landvault-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Mindzvue-Source.json --iam-account=Mindzvue-Source@mindzvue-source.iam.gserviceaccount.com --project=mindzvue-source
+gcloud iam service-accounts keys create Landvault-Source.json --iam-account=Landvault-Source@landvault-source.iam.gserviceaccount.com --project=landvault-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=mindzvue-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com --project=landvault-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Mindzvue-Source.json
+cloudshell download Landvault-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=883905654251
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=96558887780
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
