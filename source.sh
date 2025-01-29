f7f5f02bce43d65bd893801c22c935434d8e3a27
RaglemL <133904396+RaglemL@users.noreply.github.com>
2025-01-29 17:43:45 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create foundationsmusic-source --name="Foundationsmusic-Source"
+gcloud projects create obsessvr-source --name="Obsessvr-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project foundationsmusic-source
+gcloud config set project obsessvr-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 324162654638 --member="user:cloudasta@foundationsmusic.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 481388886833 --member="user:cloudasta@obsessvr.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 324162654638 --member="user:cloudasta@foundationsmusic.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 481388886833 --member="user:cloudasta@obsessvr.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=324162654638
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=481388886833
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Foundationsmusic-Source --project=foundationsmusic-source
+gcloud iam service-accounts create Obsessvr-Source --project=obsessvr-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding foundationsmusic-source --member="serviceAccount:Foundationsmusic-Source@foundationsmusic-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding obsessvr-source --member="serviceAccount:Obsessvr-Source@obsessvr-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Foundationsmusic-Source@foundationsmusic-source.iam.gserviceaccount.com --project=foundationsmusic-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Obsessvr-Source@obsessvr-source.iam.gserviceaccount.com --project=obsessvr-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Foundationsmusic-Source.json --iam-account=Foundationsmusic-Source@foundationsmusic-source.iam.gserviceaccount.com --project=foundationsmusic-source
+gcloud iam service-accounts keys create Obsessvr-Source.json --iam-account=Obsessvr-Source@obsessvr-source.iam.gserviceaccount.com --project=obsessvr-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=foundationsmusic-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=obsessvr-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Foundationsmusic-Source.json
+cloudshell download Obsessvr-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=324162654638
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=481388886833
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
