92b9b2d4ae0ee3b4e697c562e162db89ce9cd80f
RaglemL <133904396+RaglemL@users.noreply.github.com>
2025-03-07 12:46:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create melapela-source --name="Melapela-Source"
+gcloud projects create vapeclubsv-source --name="Vapeclubsv-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project melapela-source
+gcloud config set project vapeclubsv-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 481388886833 --member="user:cloudasta@melapela.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 1090329255832 --member="user:paola@vapeclubsv.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 481388886833 --member="user:cloudasta@melapela.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 1090329255832 --member="user:paola@vapeclubsv.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=481388886833
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=1090329255832
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Melapela-Source --project=melapela-source
+gcloud iam service-accounts create Vapeclubsv-Source --project=vapeclubsv-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding melapela-source --member="serviceAccount:Melapela-Source@melapela-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding vapeclubsv-source --member="serviceAccount:Vapeclubsv-Source@vapeclubsv-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Melapela-Source@melapela-source.iam.gserviceaccount.com --project=melapela-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Vapeclubsv-Source@vapeclubsv-source.iam.gserviceaccount.com --project=vapeclubsv-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Melapela-Source.json --iam-account=Melapela-Source@melapela-source.iam.gserviceaccount.com --project=melapela-source
+gcloud iam service-accounts keys create Vapeclubsv-Source.json --iam-account=Vapeclubsv-Source@vapeclubsv-source.iam.gserviceaccount.com --project=vapeclubsv-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=melapela-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=vapeclubsv-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Melapela-Source.json
+cloudshell download Vapeclubsv-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=481388886833
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=1090329255832
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
