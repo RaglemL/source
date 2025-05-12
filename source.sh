0c31be9db9d94ac122b4a7b62935ab1f8d390a7f
RaglemL <133904396+RaglemL@users.noreply.github.com>
2025-05-12 13:41:43 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,19 +2,19 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create seated-source --name="Seated-Source"
+gcloud projects create napster-source --name="Napster-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project seated-source
+gcloud config set project napster-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 317184365945 --member="user:cloudasta@seated.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 317184365945 --member="user:cloudasta@napster.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 317184365945 --member="user:cloudasta@seated.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 317184365945 --member="user:cloudasta@napster.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
@@ -24,32 +24,32 @@ sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Seated-Source --project=seated-source
+gcloud iam service-accounts create Napster-Source --project=napster-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding seated-source --member="serviceAccount:Seated-Source@seated-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding napster-source --member="serviceAccount:Napster-Source@napster-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Seated-Source@seated-source.iam.gserviceaccount.com --project=seated-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Napster-Source@napster-source.iam.gserviceaccount.com --project=napster-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Seated-Source.json --iam-account=Seated-Source@seated-source.iam.gserviceaccount.com --project=seated-source
+gcloud iam service-accounts keys create Napster-Source.json --iam-account=Napster-Source@napster-source.iam.gserviceaccount.com --project=napster-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=seated-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=napster-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Seated-Source.json
+cloudshell download Napster-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
