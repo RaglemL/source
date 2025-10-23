a7e7db5f76e6609a5e308251d8a8d01c39071b3f
RaglemL <133904396+RaglemL@users.noreply.github.com>
2025-10-23 11:06:47 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -12,14 +12,14 @@ sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 396232685914 --member="user:cloudasta.admin@dwellinspectaz.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 626606310801 --member="user:cloudasta.admin@dwellinspectaz.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 396232685914 --member="user:cloudasta.admin@dwellinspectaz.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 626606310801 --member="user:cloudasta.admin@dwellinspectaz.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=396232685914
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=626606310801
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
@@ -54,7 +54,7 @@ sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=396232685914
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=626606310801
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
