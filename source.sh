d27c22be8c14c783b1fd5f066dc90f72b59ecb1c
RaglemL <133904396+RaglemL@users.noreply.github.com>
2026-01-21 10:50:46 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,19 +2,19 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create teamscott-source --name="Teamscott-Source"
+gcloud projects create carryoutsupplies-source --name="Carryoutsupplies-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project teamscott-source
+gcloud config set project carryoutsupplies-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 831508015057 --member="user:cloudasta.admin@teamscott.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 831508015057 --member="user:cloudasta@carryoutsupplies.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 831508015057 --member="user:cloudasta.admin@teamscott.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 831508015057 --member="user:cloudasta@carryoutsupplies.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
@@ -24,32 +24,32 @@ sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Teamscott-Source --project=teamscott-source
+gcloud iam service-accounts create Carryoutsupplies-Source --project=carryoutsupplies-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding teamscott-source --member="serviceAccount:Teamscott-Source@teamscott-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding carryoutsupplies-source --member="serviceAccount:Carryoutsupplies-Source@carryoutsupplies-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Teamscott-Source@teamscott-source.iam.gserviceaccount.com --project=teamscott-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Carryoutsupplies-Source@carryoutsupplies-source.iam.gserviceaccount.com --project=carryoutsupplies-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Teamscott-Source.json --iam-account=Teamscott-Source@teamscott-source.iam.gserviceaccount.com --project=teamscott-source
+gcloud iam service-accounts keys create Carryoutsupplies-Source.json --iam-account=Carryoutsupplies-Source@carryoutsupplies-source.iam.gserviceaccount.com --project=carryoutsupplies-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=teamscott-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=carryoutsupplies-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Teamscott-Source.json
+cloudshell download Carryoutsupplies-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
