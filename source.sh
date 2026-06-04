d61d69fea78eb9bee277e6a179f9a823677d5c75
RaglemL <133904396+RaglemL@users.noreply.github.com>
2026-06-04 10:09:44 -0600

    Automated update from Google Sheets

--- source.sh
+++ source.sh
@@ -2,59 +2,59 @@
 
 # Create a new Google Cloud project
 echo "[Creating GCP Project...]"
-gcloud projects create carryoutsupplies-source --name="Carryoutsupplies-Source"
+gcloud projects create pro-spectde-source --name="Pro-Spectde-Source"
 sleep 5 # Adding a delay of 5 seconds
 
 # Set the current Google Cloud project
 echo "[Setting up GCP Project...]"
-gcloud config set project carryoutsupplies-source
+gcloud config set project pro-spectde-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Provide yourself Organization Policy Administrator and Project Creator roles
 echo "[Assigning Roles...]"
-gcloud organizations add-iam-policy-binding 831508015057 --member="user:cloudasta@carryoutsupplies.com" --role="roles/orgpolicy.policyAdmin"
+gcloud organizations add-iam-policy-binding 237328277798 --member="user:cloudasta@pro-spectde.com" --role="roles/orgpolicy.policyAdmin"
 sleep 5 # Adding a delay of 5 seconds
-gcloud organizations add-iam-policy-binding 831508015057 --member="user:cloudasta@carryoutsupplies.com" --role="roles/resourcemanager.projectCreator"
+gcloud organizations add-iam-policy-binding 237328277798 --member="user:cloudasta@pro-spectde.com" --role="roles/resourcemanager.projectCreator"
 sleep 5 # Adding a delay of 5 seconds
 
 # Disable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Disabling Policy Enforcement...]"
-gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=831508015057
+gcloud resource-manager org-policies disable-enforce iam.disableServiceAccountKeyCreation --organization=237328277798
 sleep 60 # Adding a delay of 60 seconds to allow propagation
 
 # Create a new service account
 echo "[Creating Service Account...]"
-gcloud iam service-accounts create Carryoutsupplies-Source --project=carryoutsupplies-source
+gcloud iam service-accounts create Pro-Spectde-Source --project=pro-spectde-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Add IAM policy binding to the project
 echo "[Adding Policies...]"
-gcloud projects add-iam-policy-binding carryoutsupplies-source --member="serviceAccount:Carryoutsupplies-Source@carryoutsupplies-source.iam.gserviceaccount.com" --role="roles/editor"
+gcloud projects add-iam-policy-binding pro-spectde-source --member="serviceAccount:Pro-Spectde-Source@pro-spectde-source.iam.gserviceaccount.com" --role="roles/editor"
 sleep 5 # Adding a delay of 5 seconds
 
 # Get the unique ID of the service account
 echo "[Obtaining Unique ID...]"
-gcloud iam service-accounts describe Carryoutsupplies-Source@carryoutsupplies-source.iam.gserviceaccount.com --project=carryoutsupplies-source --format="value(uniqueId)"
+gcloud iam service-accounts describe Pro-Spectde-Source@pro-spectde-source.iam.gserviceaccount.com --project=pro-spectde-source --format="value(uniqueId)"
 sleep 5 # Adding a delay of 5 seconds
 
 # Create a service account key and save it to a JSON file
 echo "[Creating JSON Key...]"
-gcloud iam service-accounts keys create Carryoutsupplies-Source.json --iam-account=Carryoutsupplies-Source@carryoutsupplies-source.iam.gserviceaccount.com --project=carryoutsupplies-source
+gcloud iam service-accounts keys create Pro-Spectde-Source.json --iam-account=Pro-Spectde-Source@pro-spectde-source.iam.gserviceaccount.com --project=pro-spectde-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable necessary Google services
 echo "[Enabling APIs...]"
-gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=carryoutsupplies-source
+gcloud services enable drive.googleapis.com sheets.googleapis.com admin.googleapis.com people.googleapis.com contacts.googleapis.com migrate.googleapis.com gmail.googleapis.com calendar-json.googleapis.com groupsmigration.googleapis.com groupssettings.googleapis.com tasks.googleapis.com forms.googleapis.com vault.googleapis.com storage-component.googleapis.com chat.googleapis.com --project=pro-spectde-source
 sleep 5 # Adding a delay of 5 seconds
 
 # Download the service account key JSON file
 echo "[Downloading JSON Key...]"
-cloudshell download Carryoutsupplies-Source.json
+cloudshell download Pro-Spectde-Source.json
 sleep 5 # Adding a delay of 5 seconds
 
 # Enable the constraint iam.disableServiceAccountKeyCreation enforcement
 echo "[Re-enabling Policy Enforcement...]"
-gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=831508015057
+gcloud resource-manager org-policies enable-enforce iam.disableServiceAccountKeyCreation --organization=237328277798
 sleep 5 # Adding a delay of 5 seconds
 
 # Tasks completed confirmation
